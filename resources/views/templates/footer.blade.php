995084f96a3317397de057a2e3f24852858b91fa
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-09-04 18:01:06 +0200

    footer completed

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,3 +1,115 @@
-<footer>
+<div class="blue-bar">
+    <div class="container">
+        <ul>
+            <li>
+                <a href="#">
+                    <img src="/img/buy-comics-digital-comics.png" alt="digital-comics">
+                    <div>DIGITAL COMICS</div>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/img/buy-comics-merchandise.png" alt="merchandise">
+                    <div>DC MERCHANDISE</div>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/img/buy-comics-subscriptions.png" alt="comics subscriptions">
+                    <div>SUBSCRIPTION</div>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/img/buy-comics-shop-locator.png" alt="comics shop locator">
+                    <div>COMIC SHOP LOCATOR</div>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="/img/buy-dc-power-visa.svg" alt="dc power visa">
+                    <div>DC POWER VISA</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="main-bar">
+    <div class="container">
+        <div class="left-content">
+            <div class="list-box">
+                <ul>
+                    <li>DC COMICS</li>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Video</li>
+                    <li>News</li>
+                </ul>
+                    
+                <ul>
+                    <li>SHOP</li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
     
-</footer>
+            <div class="list-box">
+                <ul>
+                    <li>DC</li>
+                    <li>Terms of use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact  us</li>
+                </ul>
+            </div>
+
+            <div class="list-box">
+                <ul>
+                    <li>SITES</li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="right-content"></div>
+    </div>
+</div>
+
+<div class="social-bar">
+    <div class="container">
+        <div class="left-content">
+            <div class="footer-button">SIGN-UP NOW!</div>
+        </div>
+
+        <div class="right-content">
+            <div>FOLLOW US</div>
+
+            <ul>
+                <li><img src="/img/footer-facebook.png" alt="fb"></li>
+                <li><img src="/img/footer-twitter.png" alt="tw"></li>
+                <li><img src="/img/footer-youtube.png" alt="yt"></li>
+                <li><img src="/img/footer-pinterest.png" alt="pi"></li>
+                <li><img src="/img/footer-periscope.png" alt="pe"></li>
+            </ul>
+        </div>
+    </div>
+</div>

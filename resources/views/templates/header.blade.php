2a0b08cbf7419cea8a5a48bcf407bcaab68ff88b
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-09-04 17:01:08 +0200

    header completed

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,3 +1,30 @@
 <header>
-    
+    <div class="container">
+        <div class="header-logo">
+            <img src="/img/dc-logo.png"" alt="dc-logo">
+        </div>
+
+        <nav>
+            <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+            </ul>
+        </nav>
+
+        <div class="search-field">
+            <input type="text" placeholder="Search">
+        
+            <i class="fas fa-search"></i>
+        </div>
+    </div>
+
+    <div class="jumbotron jumbotron-fluid"></div>
 </header>
